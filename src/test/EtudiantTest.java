71cb298a65686a1a56a2d65b3bc1606cfc0ed02a
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-16 17:15:10 +0200

    Update EtudiantTest.java

--- src/test/EtudiantTest.java
+++ src/test/EtudiantTest.java
@@ -1,23 +1,31 @@
 package test;
+import main.*;
 
 import static org.junit.Assert.*;
 
 public class EtudiantTest {
+    private Etudiant etu;
 
     @org.junit.Before
     public void setUp() throws Exception {
-
+        Formation f = new Formation();
+        f.ajouterMatiere("Maths", 2);
+        f.ajouterMatiere("Info", 5);
+        Identite id = new Identite("ETU0123", "Sousa", "Alexandre");
+        etu = new Etudiant(id, f);
     }
 
     @org.junit.Test
-    public void ajouterNote() {
+    public void testAjouterNote() {
+        etu.ajouterNote("Maths", 12);
+        assertEquals();
     }
 
     @org.junit.Test
-    public void moyenneMatiere() {
+    public void testMoyenneMatiere() {
     }
 
     @org.junit.Test
-    public void moyenneGenerale() {
+    public void testmoyenneGenerale() {
     }
 }
