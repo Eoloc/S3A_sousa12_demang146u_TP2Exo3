cccdff1ca2befbce2627943a6891d9cdfc81f9d1
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-16 16:56:57 +0200

    Create EtudiantTest.java

---
+++ src/test/EtudiantTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.Assert.*;
+
+public class EtudiantTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void ajouterNote() {
+    }
+
+    @org.junit.Test
+    public void moyenneMatiere() {
+    }
+
+    @org.junit.Test
+    public void moyenneGenerale() {
+    }
+}
