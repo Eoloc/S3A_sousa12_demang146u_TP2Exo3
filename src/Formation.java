a2ccd0b4975d153b5703ce0fe637a650bd0720e1
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-16 16:29:10 +0200

    Class Formation

--- src/Formation.java
+++ src/Formation.java
@@ -1,3 +1,35 @@
-public class Formation {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Formation {
+    private List<String> matieres;
+    private Map<String, Double> coefs;
+
+    public Formation() {
+        matieres = new ArrayList<String>();
+        coefs = new HashMap<String, Double>();
+    }
+
+    public void ajouterMatiere(String nom, double coef) {
+        if (!matieres.contains(nom)) {
+            matieres.add(nom);
+            coefs.put(nom, coef);
+        }
+    }
+
+    public void supprimerMatiere(String nom) {
+        if (matieres.contains(nom)) {
+            matieres.remove(nom);
+            coefs.remove(nom);
+        }
+    }
+
+    public double getCoef(String nom) {
+        if (matieres.contains(nom))
+            return coefs.get(nom);
+        else
+            return -1;
+    }
 }
