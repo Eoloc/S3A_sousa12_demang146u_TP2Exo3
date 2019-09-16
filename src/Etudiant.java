5e712b3b4f362779f626a4e13c04959e4cccc595
Alexandre Sousa <Alexandre.Sousa@users.noreply.github.com>
2019-09-16 16:48:01 +0200

    Ajout de la classe Etudiant

---
+++ src/Etudiant.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class Etudiant {
+    private Identite identite;
+    private Formation formation;
+    private HashMap<String, List<Double>> resultats;
+
+    public Etudiant(Identite pIdentite, Formation pFormation) {
+        this.identite = pIdentite;
+        this.formation = pFormation;
+        this.resultats = new HashMap<String, List<Double>>();
+        for (String matiere : this.formation.getMatiere()){
+            List<Double> listeNote = new List<Double>();
+            this.resultats.put(matiere, listeNote);
+        }
+        this.resultats.put(matiere, listeNote);
+    }
+
+    public void ajouterNote(String matiere, double note) {
+        if (note >= 0 && note <= 20) {
+            if (this.resultats.containsKey(matiere)) {
+                List<Double> listeNote = this.resultats.get(matiere);
+                listeNote.add(note);
+            }
+        }
+    }
+
+    public double moyenneMatiere(String matiere) {
+        double moyenne = 0;
+        if (this.resultats.containsKey(matiere)) {
+            List<Double> listeNote = this.resultats.get(matiere);
+            for (int i = 0; i < listeNote.size(); i++) {
+                moyenne += listeNote.get(i);
+            }
+            moyenne = moyenne / listeNote.size();
+        }
+        return moyenne;
+    }
+
+    public double moyenneGenerale() {
+        double moyenne = 0;
+        double totalCoef = 0;
+        for (String matiere : this.formation.getMatieres()) {
+            moyenne += moyenneMatiere(matiere) * this.formation.getCoef(matiere);
+            totalCoef += this.formation.getCoef(matiere);
+        }
+        moyenne = moyenne / totalCoef;
+        
+
+        return moyenne;
+    }
+
+}
