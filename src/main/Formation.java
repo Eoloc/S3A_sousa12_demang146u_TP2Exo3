eb499e07c833ccf6709d02f9575120c306e22bd9
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-16 17:07:39 +0200

    fix coef

--- src/main/Formation.java
+++ src/main/Formation.java
@@ -28,7 +28,7 @@ public class Formation {
         if (matieres.contains(nom))
             return coefs.get(nom);
         else
-            return -1;
+            return 0;
     }
 
     @Override
