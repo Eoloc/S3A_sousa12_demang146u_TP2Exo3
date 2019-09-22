8557b522e8d5d5e4dafbdc25a8894dfd4a9734bf
Eoloc <Eoloc@users.noreply.github.com>
2019-09-22 13:26:46 +0200

    Update TestFormation.java

--- src/test/TestFormation.java
+++ src/test/TestFormation.java
@@ -8,30 +8,53 @@ import org.junit.Test;
 import main.Formation;
 
 public class TestFormation {
+	
+	Formation formation;
 
 	@Before
 	public void setUp() throws Exception {
-		Formation formation = new Formation();
+		formation = new Formation();
+		formation.ajouterMatiere("Mathématique", 9);
+		formation.ajouterMatiere("SVT", 7);
 	}
 
 	@Test
 	public void testAjouterMatiere() {
-		
+		assertTrue("Mathématique devrait être dans la formation", formation.getMatieres().contains("Mathématique"));
 	}
 
 	@Test
 	public void testSupprimerMatiere() {
-		fail("Not yet implemented");
+		formation.supprimerMatiere("Mathématique");
+		assertTrue("SVT devrait être dans la formation", formation.getMatieres().contains("SVT"));
+		assertFalse("Mathématique ne devrait pas être dans la formation", formation.getMatieres().contains("Mathématique"));
 	}
 
 	@Test
 	public void testGetCoef() {
-		fail("Not yet implemented");
+		assertEquals("Le coeficient de Mathématique devrait être 9", 9, formation.getCoef("Mathématique"), 0.1);
+		assertEquals("Le coeficient de SVT devrait être 7", 7, formation.getCoef("SVT"), 0.1);
 	}
 
 	@Test
-	public void testEqualsObject() {
-		fail("Not yet implemented");
+	public void testEquals() {
+		Formation formation2 = new Formation();
+		formation2.ajouterMatiere("Mathématique", 9);
+		formation2.ajouterMatiere("SVT", 7);
+		
+		Formation formation3 = new Formation();
+		
+		Formation formation4 = new Formation();
+		formation4.ajouterMatiere("Mathématique", 9);
+		formation4.ajouterMatiere("SVT", 7);
+		formation4.ajouterMatiere("Histoire", 4);
+		formation4.ajouterMatiere("Géographie", 4);
+		formation4.ajouterMatiere("EPS", 1);
+		
+		assertTrue("Les formations 1 et 2 devrait être les mêmes", formation.equals(formation2));
+		assertFalse("Les formations 1 et 3 ne devrait pas être les mêmes", formation.equals(formation3));
+		assertFalse("Les formations 1 et 4 ne devrait pas être les mêmes", formation.equals(formation4));
+		assertFalse("Les formations 2 et 3 ne devrait être pas les mêmes", formation2.equals(formation3));
 	}
 
 }
