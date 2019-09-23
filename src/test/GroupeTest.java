3712299f63609db677c5764e68338103029ca097
Eoloc <Eoloc@users.noreply.github.com>
2019-09-23 17:59:59 +0200

    Completion des tests Groupe

--- src/test/GroupeTest.java
+++ src/test/GroupeTest.java
@@ -1,5 +1,9 @@
 package test;
 
+import main.Etudiant;
+import main.Formation;
+import main.Groupe;
+import main.Identite;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,14 +11,27 @@ import static org.junit.Assert.*;
 
 public class GroupeTest {
 
+    Groupe groupe;
+    Formation f;
+    Etudiant etudiant1, etudiant2;
+
     @Before
     public void setUp() throws Exception {
-
+        f = new Formation();
+        f.ajouterMatiere("Mathématique", 9);
+        f.ajouterMatiere("Informatique", 5);
+        Identite id1 = new Identite("ETU0123", "Sousa", "Alexandre");
+        Identite id2 = new Identite("ETU4567", "Demange", "Louis");
+        etudiant1 = new Etudiant(id1, f);
+        etudiant2 = new Etudiant(id2, f);
+        groupe = new Groupe(f);
+        groupe.ajouterEtu(etudiant1);
     }
 
     @Test
     public void ajouterEtu() {
 
+        assertTrue("", );
     }
 
     @Test
