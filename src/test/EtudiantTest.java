ee37afdd5351d24b3b823b61c23489f23f04c352
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-23 17:40:31 +0200

    ajout de commentaires sur les tests Etudiants

--- src/test/EtudiantTest.java
+++ src/test/EtudiantTest.java
@@ -17,25 +17,34 @@ public class EtudiantTest {
 
     @org.junit.Test
     public void testAjouterNote() {
+        /* ajout note normal */
         etu.ajouterNote("Maths", 12);
         assertEquals(12, (double) etu.getResultats().get("Maths").get(0), 0.001);
+
+        /* ajout note pas dans la formation */
         etu.ajouterNote("Eco", 5);
         assertEquals(null, etu.getResultats().get("Eco"));
     }
 
     @org.junit.Test
     public void testMoyenneMatiere() {
+        /* moyenne dans la formation */
         etu.ajouterNote("Maths", 10);
         etu.ajouterNote("Maths", 15);
         etu.ajouterNote("Maths", 20);
         assertEquals(15, etu.moyenneMatiere("Maths"), 0.001);
+
+        /* moyenne pas dans la formation */
         assertNull(etu.moyenneMatiere("Eco"));
     }
 
     @org.junit.Test
     public void testMoyenneGenerale() {
+        /* pas de note dans toute les matiere de la foramtion */
     	etu.ajouterNote("Maths", 10);
         assertEquals(10.0, etu.moyenneGenerale(), 0.001);
+
+        /* une note dans chaque matiere de la formation */
         etu.ajouterNote("Info", 20);
         assertEquals(17.143, etu.moyenneGenerale(), 0.001);
     }
