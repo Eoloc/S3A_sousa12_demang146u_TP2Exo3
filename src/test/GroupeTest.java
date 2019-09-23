b3895239d51fa6569e302a968ab8601387c4b026
Eoloc <Eoloc@users.noreply.github.com>
2019-09-23 17:35:28 +0200

    Ajout de la class testGroupe

---
+++ src/test/GroupeTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GroupeTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void ajouterEtu() {
+
+    }
+
+    @Test
+    public void supprimerEtu() {
+    }
+
+    @Test
+    public void moyenneMatiere() {
+    }
+
+    @Test
+    public void moyenneGenerale() {
+    }
+}
