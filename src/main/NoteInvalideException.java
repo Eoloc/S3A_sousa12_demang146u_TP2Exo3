32e52d5ade550ae30e617e66c23fe5d226e9f768
Louis Demange <louis.demange5@etu.univ-lorraine.fr>
2019-09-30 02:13:14 +0200

    Ajout d'exceptions pour les notes

---
+++ src/main/NoteInvalideException.java
@@ -0,0 +1,12 @@
+package main;
+
+public class NoteInvalideException extends Exception {
+
+    public NoteInvalideException() {
+        super();
+    }
+
+    public NoteInvalideException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/NoteOrFormationException.java
@@ -0,0 +1,12 @@
+package main;
+
+public class NoteOrFormationException extends Exception {
+
+    public NoteOrFormationException() {
+        super();
+    }
+
+    public NoteOrFormationException(String s) {
+        super(s);
+    }
+}
